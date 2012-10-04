32ae751f690e7e6575a3a1a1d73c05548d8c7e3b
Shou Ya <shouyatf@gmail.com>
2012-10-04 18:47:48 +0800

    separate module & selection sort

--- 2012/octo/4/BubbleSort.java
+++ 2012/octo/4/BubbleSort.java
@@ -1,4 +1,5 @@
 
+/* import CommonUtils.*; */
 
 
 public class BubbleSort {
@@ -13,25 +14,15 @@ public class BubbleSort {
 	}
 
 	System.out.println("Before sort:");
-	print_array(ary);
+	CommonUtils.print_array(ary);
 
 	bubble_sort(ary);
 
 	System.out.println("After bubble sort:");
-	print_array(ary);
+	CommonUtils.print_array(ary);
     }
 
-    public static void print_array(int[] array) {
-	System.out.print('[');
-	for (int i = 0; i != array.length; ++i) {
-	    System.out.print(array[i]);
-	    if (i != array.length - 1)
-		System.out.print(", ");
-	}
-	System.out.println(']');
-    }
-
-    public static void bubble_sort(int[] array) {
+    static void bubble_sort(int[] array) {
 	for (int i = array.length - 1; i != 0; --i) {
 	    for (int j = 0; j != i; ++j) {
 		if (array[j] > array[j + 1]) {
